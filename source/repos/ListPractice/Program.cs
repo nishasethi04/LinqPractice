988b90b9884de4f8a49a01a57d9b065f926c899b
nisethi85 <nisethi85@gmail.com>
2020-01-30 18:28:09 -0500

    Changes made

---
+++ source/repos/ListPractice/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> newlist = new List<string> { "Hello", "Hi", "Byee" };
+            Console.WriteLine("");
+            Console.WriteLine("Before Adding");
+            foreach(var item in newlist)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("");
+            Console.WriteLine("After Adding");
+            newlist.Add("New element");
+            foreach(var item in newlist)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("");
+            Console.WriteLine("After removing");
+            newlist.Remove("hi");
+            newlist.RemoveAt(1);
+            foreach (var item in newlist)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
